d095f7b7ebe2dd0f71e01be75b5373f48a7479b8
dmipet90 <dmitri.petrussevits@gmail.com>
2019-09-01 20:58:33 +0300

    Output comment added

--- src/Figure.java
+++ src/Figure.java
@@ -12,7 +12,7 @@ public class Figure {
         int count = 0;
 
         // Read each line of file
-        while(line != null) {
+        while (line != null) {
             count++;
             if (!line.isEmpty()) {
                 line = line.replaceAll("\\s", ""); // e.g from 1 2 3 to 123
@@ -51,7 +51,7 @@ public class Figure {
             } else {
 
                 // Squares permutations
-                for(int i = 0; i < n; ++i) {
+                for (int i = 0; i < n; ++i) {
                     this.replaceSquares(figure, i, n - 1);
                     this.permuteSquares(figure, n - 1);
                     this.replaceSquares(figure, i, n - 1);
@@ -78,18 +78,18 @@ public class Figure {
     private boolean checkFigure(ArrayList<Square> figure, int n) {
         int correctValue = 10;
         if ((n != 1 || figure.get(0).getTopRight() + figure.get(1).getTopLeft() <= correctValue) &&
-        (n != 1 || figure.get(0).getBottomLeft() + figure.get(3).getTopLeft() + figure.get(2).getTopRight() <= correctValue) &&
-        (n != 1 || this.fourCornerSum(figure.get(0), figure.get(1), figure.get(4), figure.get(3)) == correctValue) &&
-        (n != 1 || figure.get(1).getBottomRight() + figure.get(4).getTopRight() + figure.get(5).getTopLeft() <= correctValue) &&
-        (n != 2 || figure.get(2).getBottomLeft() + figure.get(6).getTopLeft() <= correctValue) &&
-        (n != 2 || this.fourCornerSum(figure.get(2), figure.get(3), figure.get(7), figure.get(6)) == correctValue) &&
-        (n != 3 || this.fourCornerSum(figure.get(3), figure.get(4), figure.get(8), figure.get(7)) == correctValue) &&
-        (n != 4 || this.fourCornerSum(figure.get(4), figure.get(5), figure.get(9), figure.get(8)) == correctValue) &&
-        (n != 5 || figure.get(5).getBottomRight() + figure.get(9).getTopRight() <= correctValue) &&
-        (n != 6 || figure.get(6).getBottomRight() + figure.get(7).getBottomLeft() + figure.get(10).getTopLeft() <= correctValue) &&
-        (n != 7 || this.fourCornerSum(figure.get(7), figure.get(8), figure.get(11), figure.get(10)) == correctValue) &&
-        (n != 8 || figure.get(9).getBottomLeft() + figure.get(8).getBottomRight() + figure.get(11).getTopRight() <= correctValue) &&
-        (n != 10 || figure.get(10).getBottomRight() + figure.get(11).getBottomLeft() <= correctValue)) {
+                (n != 1 || figure.get(0).getBottomLeft() + figure.get(3).getTopLeft() + figure.get(2).getTopRight() <= correctValue) &&
+                (n != 1 || this.fourCornerSum(figure.get(0), figure.get(1), figure.get(4), figure.get(3)) == correctValue) &&
+                (n != 1 || figure.get(1).getBottomRight() + figure.get(4).getTopRight() + figure.get(5).getTopLeft() <= correctValue) &&
+                (n != 2 || figure.get(2).getBottomLeft() + figure.get(6).getTopLeft() <= correctValue) &&
+                (n != 2 || this.fourCornerSum(figure.get(2), figure.get(3), figure.get(7), figure.get(6)) == correctValue) &&
+                (n != 3 || this.fourCornerSum(figure.get(3), figure.get(4), figure.get(8), figure.get(7)) == correctValue) &&
+                (n != 4 || this.fourCornerSum(figure.get(4), figure.get(5), figure.get(9), figure.get(8)) == correctValue) &&
+                (n != 5 || figure.get(5).getBottomRight() + figure.get(9).getTopRight() <= correctValue) &&
+                (n != 6 || figure.get(6).getBottomRight() + figure.get(7).getBottomLeft() + figure.get(10).getTopLeft() <= correctValue) &&
+                (n != 7 || this.fourCornerSum(figure.get(7), figure.get(8), figure.get(11), figure.get(10)) == correctValue) &&
+                (n != 8 || figure.get(9).getBottomLeft() + figure.get(8).getBottomRight() + figure.get(11).getTopRight() <= correctValue) &&
+                (n != 10 || figure.get(10).getBottomRight() + figure.get(11).getBottomLeft() <= correctValue)) {
             return true;
         } else {
             return false;
